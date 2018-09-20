2b8c8ad8e6270c45b28d7faaa1ee44efe39141b5
Grant Braught <braught@dickinson.edu>
2018-09-20 18:07:13 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -141,7 +141,7 @@ public class Calculator {
 	 *            the radius
 	 * @return the volume of a sphere with radius r.
 	 */
-	public double sphVol(double r) {
+	public static double sphVol(double r) {
 		return (4.0/3) * Math.PI * Math.pow(r, 3);
 	}
 }
